4a12457ba04f276ea5b9be8607db5ed284ac5fb1
zhangji1810 <zhangjipwn@gmail.com>
2019-07-24 00:18:23 +0800

    Add files via upload

---
+++ bin/merge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+dd   if=xx1  of=need_password1.rar   bs=1 count=14636234;
+dd   if=xx2  of=need_password1.rar   bs=1 count=14636234 seek=14636234;
+dd   if=xx3  of=need_password1.rar   bs=1 count=14636234 seek=29272468;
+dd   if=xx4  of=need_password1.rar   bs=1 count=14636234 seek=43908702;
+dd   if=xx5  of=need_password1.rar   bs=1 count=14636234 seek=58544936;
+dd   if=xx6  of=need_password1.rar   bs=1 count=14636234 seek=73181170;
+dd   if=xx7  of=need_password1.rar   bs=1 count=14636234 seek=87817404;
+dd   if=xx8  of=need_password1.rar   bs=1 count=14636234 seek=102453638;
+dd   if=xx9  of=need_password1.rar   bs=1 count=14636234 seek=117089872;
+dd   if=xx10 of=need_password1.rar   bs=1 count=14636234 seek=131726106;
+dd   if=xx11 of=need_password1.rar   bs=1 count=14636234 seek=146362340;
+dd   if=xx12 of=need_password1.rar   bs=1 count=14636234 seek=160998574;
+dd   if=xx13 of=need_password1.rar   bs=1 count=14636234 seek=175634808;
+dd   if=xx14 of=need_password1.rar   bs=1 count=14636234 seek=190271042;
+dd   if=xx15 of=need_password1.rar   bs=1 count=14636234 seek=204907276;
+dd   if=xx16 of=need_password1.rar   bs=1 count=13 seek=219543510;
---
+++ bin/test1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+dd if=need_password.rar  of=xx1 bs=1 count=14636234;
+dd if=need_password.rar  of=xx2 bs=1 count=14636234 skip=14636234;
+dd if=need_password.rar  of=xx3 bs=1 count=14636234 skip=29272468;
+dd if=need_password.rar  of=xx4 bs=1 count=14636234 skip=43908702;
+dd if=need_password.rar  of=xx5 bs=1 count=14636234 skip=58544936;
+dd if=need_password.rar  of=xx6 bs=1 count=14636234 skip=73181170;
+dd if=need_password.rar  of=xx7 bs=1 count=14636234 skip=87817404;
+dd if=need_password.rar  of=xx8 bs=1 count=14636234 skip=102453638;
+dd if=need_password.rar  of=xx9 bs=1 count=14636234 skip=117089872;
+dd if=need_password.rar  of=xx10 bs=1 count=14636234 skip=131726106;
+dd if=need_password.rar  of=xx11 bs=1 count=14636234 skip=146362340;
+dd if=need_password.rar  of=xx12 bs=1 count=14636234 skip=160998574;
+dd if=need_password.rar  of=xx13 bs=1 count=14636234 skip=175634808;
+dd if=need_password.rar  of=xx14 bs=1 count=14636234 skip=190271042;
+dd if=need_password.rar  of=xx15 bs=1 count=14636234 skip=204907276;
+dd if=need_password.rar  of=xx16 bs=1 count=13 skip=219543510;
